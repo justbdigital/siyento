df0219a7a5cb4c55bf7afe860419cf7fb3003c62
justbdigital <justin@justb.uk>
2015-09-14 16:29:13 +0100

    first heroku deployment

---
+++ app.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+require 'sinatra'
+require 'nokogiri'
+require 'open-uri'
+
+get '/' do
+
+#Suredeal Groupon Variables
+gurl = "http://suredeal.com.ph/manila/all-deals/beeconomic/?order=cheapest-deals"
+gdata = Nokogiri::HTML(open(gurl))
+@gdeals = gdata.css('.deal-container-large')
+
+#Suredeal Tcat Variables
+turl = "http://suredeal.com.ph/manila/shopping-and-product-deals/tcat/?order=cheapest-deals"
+tdata = Nokogiri::HTML(open(turl))
+@tdeals = tdata.css('.deal-container-large')
+
+#Suredeal Metro Deals Variables
+murl = "http://suredeal.com.ph/manila/all-deals/metrodeal/?order=cheapest-deals"
+mdata = Nokogiri::HTML(open(murl))
+@mdeals = mdata.css('.deal-container-large')
+ 
+  erb :index
+
+end
